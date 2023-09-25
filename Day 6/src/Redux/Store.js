1bde6e4ccbd01393789b3d059900ac4a7f7d1edd
Vivinprabhu-S <125637510+Vivinprabhu-S@users.noreply.github.com>
2023-09-25 09:32:17 +0530

    Delete Day 6 directory

--- Day 6/src/Redux/Store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { userSlice } from "../Redux/UserSlice";
-
-
-export default configureStore({
-    reducer: {
-        user:userSlice.reducer,
-    }
-})
